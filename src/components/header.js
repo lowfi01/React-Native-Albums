1df89b4faf39c10dd1d15dc526e95108bac0e5a0
DESKTOP-85TAR51\james <jamesherrero@gmail.com>
2018-02-21 23:34:08 +1100

    Styling and Header Component

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,44 @@
 // Import Libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Platform } from 'react-native';
 
 // Make a component
-const Header = () => {
-  const { textStyle } = styles;
+const Header = ({ headerText }) => {
+  const { textStyle, viewStyle } = styles;
 
-  return <Text style={textStyle}>Albums</Text>;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{headerText}</Text>
+    </View>
+  );
 };
 
 // Styling
-
 const styles = {
   textStyle: {
     fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    borderRadius: 5,
+    paddingTop: 5,
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    ...Platform.select({
+      ios: {
+        shadowOpacity: 0.2
+      },
+      android: {
+        elevation: 2
+      }
+    }),
+    position: 'relative'
   }
 };
 
