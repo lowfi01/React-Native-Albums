ec0ff77ea286ceafd70305f0ad9044d6be3b0b0f
DESKTOP-85TAR51\james <jamesherrero@gmail.com>
2018-02-24 18:30:14 +1100

    Card Component styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,31 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
 
-const Card = () => <View />;
+const Card = () => <View style={styles.container} />;
+
+const styles = {
+  container: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#ddd',
+    borderBottom: 0,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    ...Platform.select({
+      ios: {
+        shadowOpacity: 0.2
+      },
+      android: {
+        elevation: 2
+      }
+    }),
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10
+  }
+};
 
 export default Card;
